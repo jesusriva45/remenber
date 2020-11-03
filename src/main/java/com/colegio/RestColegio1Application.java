0291d3b5a8f7d5828fccb15e65be157482262477
jesusriva45 <jesusriva45@gmail.com>
2020-11-02 20:18:49 -0500

    Push

---
+++ src/main/java/com/colegio/RestColegio1Application.java
@@ -0,0 +1,13 @@
+package com.colegio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestColegio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestColegio1Application.class, args);
+	}
+
+}
